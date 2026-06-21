b815ea4b83553d731ad97ff6215dabada0bee51f
KOCA Ali Sinan <kocaalisinan@Okans-MacBook-Pro-2.local>
2026-06-21 20:54:20 +0200

    ...

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -24,10 +24,20 @@ CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 
-FLUSH PRIVILEGES;
 EOF
 	echo "Database initialized."
 fi
 
 echo "Starting MariaDB..."
 exec mariadbd --user=mysql
+
+# PID = Process ID, every running process gets a number in linux.
+# ps -ef
+# In a Docker container, the main service should become PID 1.
+# Docker sends signals (SIGTERM) to PID 1 when stopping the container.
+# Using exec replaces the shell with mariadbd so MariaDB becomes PID 1.
+# Works since its not a VM, so the PID isn't to launch but to do whatever we docker run
+
+# We want the volume to be accesible even later so if we change the image
+# or if MariaDB crashes its gonna restart but
+# we still keep the collected data and don't lose it.
